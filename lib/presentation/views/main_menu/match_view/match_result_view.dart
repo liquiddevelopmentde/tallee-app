e909f347e316d2a9b44d64695f1fce52d184feae
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-03-06 22:11:15 +0100

    Removed unused function

--- lib/presentation/views/main_menu/match_view/match_result_view.dart
+++ lib/presentation/views/main_menu/match_view/match_result_view.dart
@@ -148,21 +148,4 @@ class _MatchResultViewState extends State<MatchResultView> {
     }
     widget.onWinnerChanged?.call();
   }
-
-  /// Retrieves all players associated with the given [match].
-  /// This includes players directly assigned to the match
-  /// as well as members of the group (if any).
-  /// The returned list is sorted alphabetically by player name.
-  List<Player> getAllPlayers(Match match) {
-    List<Player> players = [];
-
-    if (match.group == null) {
-      players = [...match.players];
-    } else {
-      players = [...match.players, ...match.group!.members];
-    }
-
-    players.sort((a, b) => a.name.compareTo(b.name));
-    return players;
-  }
 }
