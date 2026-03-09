3fe421676c0ed1aad66d418b47ba524d3da2ae7d
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-03-09 15:29:18 +0100

    update onDelete behavior for groupId in match_table to set null

--- lib/data/db/tables/match_table.dart
+++ lib/data/db/tables/match_table.dart
@@ -7,8 +7,10 @@ class MatchTable extends Table {
   TextColumn get gameId =>
       text().references(GameTable, #id, onDelete: KeyAction.cascade)();
   // Nullable if there is no group associated with the match
-  TextColumn get groupId =>
-      text().references(GroupTable, #id, onDelete: KeyAction.cascade).nullable()();
+  // onDelete: If a group gets deleted, groupId in the match gets set to null
+  TextColumn get groupId => text()
+      .references(GroupTable, #id, onDelete: KeyAction.setNull)
+      .nullable()();
   TextColumn get name => text().nullable()();
   TextColumn get notes => text().nullable()();
   DateTimeColumn get createdAt => dateTime()();
