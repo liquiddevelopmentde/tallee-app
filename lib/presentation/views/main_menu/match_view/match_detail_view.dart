350c5430a4bb607009c694748871ea6ffe4f01a4
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-05-09 12:57:42 +0200

    implement updateMatchStateAfterSave to refresh match scores

--- lib/presentation/views/main_menu/match_view/match_detail_view.dart
+++ lib/presentation/views/main_menu/match_view/match_detail_view.dart
@@ -205,7 +205,7 @@ class _MatchDetailViewState extends State<MatchDetailView> {
                             match: match,
                             onWinnerChanged: () {
                               widget.onMatchUpdate.call();
-                              setState(() {});
+                              updateMatchStateAfterSave();
                             },
                           ),
                         ),
@@ -228,6 +228,17 @@ class _MatchDetailViewState extends State<MatchDetailView> {
       match = editedMatch;
     });
     widget.onMatchUpdate.call();
+  }
+
+  /// Updates the match scores after saving in MatchResultView
+  Future<void> updateMatchStateAfterSave() async {
+    final scores = await db.scoreEntryDao.getAllMatchScores(matchId: match.id);
+
+    if (!mounted) return;
+
+    setState(() {
+      match.scores = scores;
+    });
   }
 
   /// Returns the widget to be displayed in the result [InfoTile]
