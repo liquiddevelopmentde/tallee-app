f0ed8c2da7ce39a530f975c9d9809b52e1fec671
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-06-09 10:37:21 +0200

    style: sort table files

--- lib/data/db/tables/group_table.dart
+++ lib/data/db/tables/group_table.dart
@@ -2,9 +2,9 @@ import 'package:drift/drift.dart';
 
 class GroupTable extends Table {
   TextColumn get id => text()();
+  DateTimeColumn get createdAt => dateTime()();
   TextColumn get name => text()();
   TextColumn get description => text()();
-  DateTimeColumn get createdAt => dateTime()();
 
   @override
   Set<Column<Object>> get primaryKey => {id};
--- lib/data/db/tables/team_table.dart
+++ lib/data/db/tables/team_table.dart
@@ -2,8 +2,8 @@ import 'package:drift/drift.dart';
 
 class TeamTable extends Table {
   TextColumn get id => text()();
-  TextColumn get name => text()();
   DateTimeColumn get createdAt => dateTime()();
+  TextColumn get name => text()();
   TextColumn get color => text().withDefault(const Constant('blue'))();
   IntColumn get score => integer().nullable()();
 
