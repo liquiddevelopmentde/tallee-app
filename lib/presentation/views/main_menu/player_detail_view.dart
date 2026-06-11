8b5e27421baa5c9fcf34f1c142d4961d53270668
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-06-12 00:59:44 +0200

    removed unused import

--- lib/presentation/views/main_menu/player_detail_view.dart
+++ lib/presentation/views/main_menu/player_detail_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:tallee/core/adaptive_page_route.dart';
-import 'package:tallee/core/common.dart';
 import 'package:tallee/core/custom_theme.dart';
 import 'package:tallee/core/enums.dart';
 import 'package:tallee/core/name_display.dart';
