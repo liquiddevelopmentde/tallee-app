daf089218f8514e9185c231bdb1f662085519bc1
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-06-15 16:53:20 +0200

    add name to pair object

--- lib/presentation/widgets/player_selection.dart
+++ lib/presentation/widgets/player_selection.dart
@@ -417,7 +417,10 @@ class _PlayerSelectionState extends State<PlayerSelection> {
     // Add new merged unit (max 2 players)
     selectedUnits.insert(
       0,
-      Team(name: '', members: allMembers.take(2).toList()),
+      Team(
+        name: '${allMembers[0].name} & ${allMembers[1].name} (unit)',
+        members: allMembers.take(2).toList(),
+      ),
     );
 
     pairingSelection.clear();
