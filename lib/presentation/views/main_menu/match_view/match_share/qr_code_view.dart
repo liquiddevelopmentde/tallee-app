7f424def7b6e6a7c6e10835c6030a2767407a1f0
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-06-16 15:49:44 +0200

    add enabledServerSharing param and implement TopCenteredMessage for disabled state

--- lib/presentation/views/main_menu/match_view/match_share/qr_code_view.dart
+++ lib/presentation/views/main_menu/match_view/match_share/qr_code_view.dart
@@ -3,6 +3,7 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 import 'package:pretty_qr_code/pretty_qr_code.dart';
 import 'package:tallee/core/custom_theme.dart';
+import 'package:tallee/presentation/widgets/top_centered_message.dart';
 
 class QrCodeView extends StatelessWidget {
   const QrCodeView({
@@ -11,12 +12,14 @@ class QrCodeView extends StatelessWidget {
     required this.isLoading,
     required this.secondsRemaining,
     required this.totalSeconds,
+    required this.enableServerSharing,
   });
 
   final QrImage? qrImage;
   final bool isLoading;
   final int secondsRemaining;
   final int totalSeconds;
+  final bool enableServerSharing;
 
   QrImage loadingStateQr() {
     final qrCode = QrCode.fromData(
@@ -33,85 +36,102 @@ class QrCodeView extends StatelessWidget {
     final int minutes = secondsRemaining ~/ 60;
     final int seconds = secondsRemaining % 60;
 
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-          child: CustomPaint(
-            foregroundPainter: CountdownPainter(
-              progress: progress,
-              color: CustomTheme.primaryColor,
-            ),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: CustomTheme.standardBorderRadiusAll,
-              ),
-              padding: const EdgeInsets.all(4),
-              child: ClipRRect(
-                borderRadius: CustomTheme.standardBorderRadiusAll,
-                child: Container(
-                  color: Colors.white,
-                  padding: const EdgeInsets.all(10),
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      Opacity(
-                        opacity: (isLoading || qrImage == null) ? 0.3 : 1.0,
-                        child: PrettyQrView(
-                          qrImage: qrImage ?? loadingStateQr(),
-                          decoration: const PrettyQrDecoration(
-                            shape: PrettyQrSquaresSymbol(),
-                            background: Colors.white,
-                          ),
+    return !enableServerSharing
+        ? TopCenteredMessage(
+            title: "Server sharing is disabled",
+            message: "Go to the settings to manually enable it.",
+            icon: Icons.close,
+          )
+        : Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 30,
+                  vertical: 10,
+                ),
+                child: CustomPaint(
+                  foregroundPainter: CountdownPainter(
+                    progress: progress,
+                    color: CustomTheme.primaryColor,
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: CustomTheme.standardBorderRadiusAll,
+                    ),
+                    padding: const EdgeInsets.all(4),
+                    child: ClipRRect(
+                      borderRadius: CustomTheme.standardBorderRadiusAll,
+                      child: Container(
+                        color: Colors.white,
+                        padding: const EdgeInsets.all(10),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            Opacity(
+                              opacity: (isLoading || qrImage == null)
+                                  ? 0.3
+                                  : 1.0,
+                              child: PrettyQrView(
+                                qrImage: qrImage ?? loadingStateQr(),
+                                decoration: const PrettyQrDecoration(
+                                  shape: PrettyQrSquaresSymbol(),
+                                  background: Colors.white,
+                                ),
+                              ),
+                            ),
+                            if (isLoading)
+                              const Center(
+                                child: CircularProgressIndicator(
+                                  color: CustomTheme.primaryColor,
+                                  strokeWidth: 5,
+                                ),
+                              ),
+                          ],
                         ),
                       ),
-                      if (isLoading)
-                        const Center(
-                          child: CircularProgressIndicator(
-                            color: CustomTheme.primaryColor,
-                            strokeWidth: 5,
-                          ),
-                        ),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ),
-        ),
-        Text(
-          minutes == 0 && seconds == 0
-              ? 'QR Code expired'
-              : 'Expires in ${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}',
-          style: const TextStyle(
-            color: CustomTheme.textColor,
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
-          ),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          width: double.infinity,
-          decoration: BoxDecoration(
-            color: CustomTheme.onBoxColor,
-            border: Border.all(color: CustomTheme.boxBorderColor, width: 2),
-            borderRadius: CustomTheme.standardBorderRadiusAll,
-          ),
-          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
-          child: const Text(
-            'Scan the QR Code with another Tallee instance to share the match.',
-            style: TextStyle(
-              color: CustomTheme.textColor,
-              fontSize: 14,
-              overflow: TextOverflow.visible,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-      ],
-    );
+              Text(
+                minutes == 0 && seconds == 0
+                    ? 'QR Code expired'
+                    : 'Expires in ${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}',
+                style: const TextStyle(
+                  color: CustomTheme.textColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: CustomTheme.onBoxColor,
+                  border: Border.all(
+                    color: CustomTheme.boxBorderColor,
+                    width: 2,
+                  ),
+                  borderRadius: CustomTheme.standardBorderRadiusAll,
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 5,
+                  vertical: 10,
+                ),
+                child: const Text(
+                  'Scan the QR Code with another Tallee instance to share the match.',
+                  style: TextStyle(
+                    color: CustomTheme.textColor,
+                    fontSize: 14,
+                    overflow: TextOverflow.visible,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          );
   }
 }
 
