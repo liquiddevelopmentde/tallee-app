56554a34c6976e84d81ec3bce9dc534d53efdbc0
Mathis Kirchner <mathis.kirchner.mk@gmail.com>
2026-06-17 14:12:02 +0200

    add fileImport to MatchShareService

--- lib/services/match_share_service.dart
+++ lib/services/match_share_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:typed_data';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:json_schema/json_schema.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:tallee/core/constants.dart';
 import 'package:tallee/data/models/models.dart';
 import 'package:tallee/services/share_exceptions.dart';
 
@@ -91,5 +94,98 @@ class MatchShareService {
       bytes: fileBytes,
     );
     // TODO: add locs
+  }
+
+  Future<(ImportResult, Match?)> chooseFileToImport() async {
+    final path = await FilePicker.pickFiles(
+      allowMultiple: false,
+      type: FileType.custom,
+      allowedExtensions: ['tallee'],
+    );
+
+    if (path == null || path.files.isEmpty) {
+      return (ImportResult.canceled, null);
+    }
+
+    try {
+      final jsonString = await _readFileContent(path.files.single);
+      if (jsonString == null) return (ImportResult.fileReadError, null);
+
+      final isValid = await validateJsonSchema(jsonString);
+      if (!isValid) return (ImportResult.invalidSchema, null);
+
+      final decoded = json.decode(jsonString) as Map<String, dynamic>;
+
+      if (!validateContent(decoded)) {
+        return (ImportResult.invalidData, null);
+      }
+
+      return (ImportResult.success, Match.fromJson(decoded));
+    } on FormatException catch (e, stack) {
+      print('[importData] FormatException');
+      print('[importData] $e');
+      print(stack);
+      return (ImportResult.formatException, null);
+    } on Exception catch (e, stack) {
+      print('[importData] Exception');
+      print('[importData] $e');
+      print(stack);
+      return (ImportResult.unknownException, null);
+    }
+  }
+
+  /// Helper method to read file content from either bytes or path
+  static Future<String?> _readFileContent(PlatformFile file) async {
+    if (file.bytes != null) return utf8.decode(file.bytes!);
+    if (file.path != null) return await File(file.path!).readAsString();
+    return null;
+  }
+
+  /// Validates the given JSON string against the schema
+  /// in `assets/app_schema.json`.
+  @visibleForTesting
+  static Future<bool> validateJsonSchema(String jsonString) async {
+    final String schemaString;
+
+    schemaString = await rootBundle.loadString('assets/match_schema.json');
+
+    try {
+      final schema = JsonSchema.create(json.decode(schemaString));
+      final jsonData = json.decode(jsonString);
+      final result = schema.validate(jsonData);
+
+      if (result.isValid) {
+        return true;
+      }
+      return false;
+    } catch (e, stack) {
+      print('[validateJsonSchema] $e');
+      print(stack);
+      return false;
+    }
+  }
+
+  /// Validates field lengths against the defined constants.
+  @visibleForTesting
+  static bool validateContent(Map<String, dynamic> decoded) {
+    // Validate match name
+    final name = decoded['name'] as String?;
+    if (name != null && name.length > Constants.MAX_MATCH_NAME_LENGTH) {
+      return false;
+    }
+
+    // Validate teams
+    final teams = decoded['teams'] as List<dynamic>?;
+    if (teams != null) {
+      for (final t in teams) {
+        final teamName = t['name'] as String?;
+        if (teamName != null &&
+            teamName.length > Constants.MAX_TEAM_NAME_LENGTH) {
+          return false;
+        }
+      }
+    }
+
+    return true;
   }
 }
