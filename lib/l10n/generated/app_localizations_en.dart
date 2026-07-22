ba5f85abe02a5506b78d24ce54dd24b156750f43
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-07-22 14:11:48 +0200

    fix: button typo

--- lib/l10n/generated/app_localizations_en.dart
+++ lib/l10n/generated/app_localizations_en.dart
@@ -1,5 +1,6 @@
 // ignore: unused_import
 import 'package:intl/intl.dart' as intl;
+
 import 'app_localizations.dart';
 
 // ignore_for_file: type=lint
@@ -94,7 +95,7 @@ class AppLocalizationsEn extends AppLocalizations {
   String get create_group => 'Create Group';
 
   @override
-  String get create_match => 'Create match';
+  String get create_match => 'Create Match';
 
   @override
   String get create_new_group => 'Create new group';
