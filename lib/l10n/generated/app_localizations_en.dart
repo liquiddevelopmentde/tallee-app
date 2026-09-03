0fa620a92c9d00e75db38f6acee0462ad98c96d9
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-03 10:27:08 +0200

    fix: loc

--- lib/l10n/generated/app_localizations_en.dart
+++ lib/l10n/generated/app_localizations_en.dart
@@ -328,8 +328,8 @@ class AppLocalizationsEn extends AppLocalizations {
     String _temp0 = intl.Intl.pluralLogic(
       count,
       locale: localeName,
-      other: 'lives',
-      one: 'life',
+      other: 'Lives',
+      one: 'Life',
     );
     return '$_temp0';
   }
