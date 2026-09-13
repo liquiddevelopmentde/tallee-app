da099d1c739b48269c7ec921c9b55b4bdbe6dd49
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-13 21:51:13 +0200

    Removed wrong bundle id [skip ci]

--- lib/presentation/views/main_menu/custom_navigation_bar.dart
+++ lib/presentation/views/main_menu/custom_navigation_bar.dart
@@ -253,7 +253,6 @@ class _CustomNavigationBarState extends State<CustomNavigationBar>
     final newVersionPlus = NewVersionPlus(
       iOSAppStoreCountry: 'de',
       androidPlayStoreCountry: 'de',
-      iOSId: 'de.felixkirchner.cabocounter',
     );
 
     VersionStatus? status;
