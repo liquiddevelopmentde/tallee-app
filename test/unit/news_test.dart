a133b4d993e395b1776dd1713067ddf4f7531703
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-16 21:07:05 +0200

    Refactored news - Added parameter localizedHeading - Added parameter localizedText - Adjusted tests - Moved news item to data/dto

--- test/unit/news_test.dart
+++ test/unit/news_test.dart
@@ -6,26 +6,26 @@ void main() {
   group('News tests', () {
     test('Fallback news (english) are set', () {
       const fallbackLoc = 'en';
-      final news = localizedNews[fallbackLoc];
-      expect(
-        news,
-        isNotNull,
-        reason: 'Fallback news for locale $fallbackLoc is null',
-      );
-      expect(
-        news,
-        isNotEmpty,
-        reason: 'Fallback news for locale $fallbackLoc is empty',
-      );
+      expect(news, isNotEmpty, reason: 'News list is empty');
 
-      for (final entry in news!) {
+      for (final item in news) {
         expect(
-          entry.heading,
-          isNotEmpty,
-          reason: 'Fallback news heading for locale $fallbackLoc is empty',
+          item.localizedHeading[fallbackLoc],
+          isNotNull,
+          reason: 'Fallback news title for locale $fallbackLoc is null',
         );
         expect(
-          entry.text,
+          item.localizedHeading[fallbackLoc],
+          isNotEmpty,
+          reason: 'Fallback news title for locale $fallbackLoc is empty',
+        );
+        expect(
+          item.localizedText[fallbackLoc],
+          isNotNull,
+          reason: 'Fallback news text for locale $fallbackLoc is null',
+        );
+        expect(
+          item.localizedText[fallbackLoc],
           isNotEmpty,
           reason: 'Fallback news text for locale $fallbackLoc is empty',
         );
@@ -37,9 +37,28 @@ void main() {
           .map((e) => e.languageCode)
           .toList();
       for (final locale in loc) {
-        final news = localizedNews[locale];
-        expect(news, isNotNull, reason: 'News for locale $locale is null');
-        expect(news, isNotEmpty, reason: 'News for locale $locale is empty');
+        for (final entry in news) {
+          expect(
+            entry.localizedHeading[locale],
+            isNotNull,
+            reason: 'News title for locale $locale is null',
+          );
+          expect(
+            entry.localizedHeading[locale],
+            isNotEmpty,
+            reason: 'News title for locale $locale is empty',
+          );
+          expect(
+            entry.localizedText[locale],
+            isNotNull,
+            reason: 'News text for locale $locale is null',
+          );
+          expect(
+            entry.localizedText[locale],
+            isNotEmpty,
+            reason: 'News text for locale $locale is empty',
+          );
+        }
       }
     });
   });
