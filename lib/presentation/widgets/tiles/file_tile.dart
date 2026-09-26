14eb566ae44fcb141840bad80c829a60716135f0
Felix Kirchner <felix.kirchner.fk@gmail.com>
2026-09-26 13:50:34 +0200

    fix: file extension duplicate

--- lib/presentation/widgets/tiles/file_tile.dart
+++ lib/presentation/widgets/tiles/file_tile.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:tallee/core/common.dart';
-import 'package:tallee/core/constants/value_constants.dart';
+import 'package:tallee/core/constants/constants.dart';
 import 'package:tallee/core/custom_theme.dart';
 import 'package:tallee/data/models/models.dart';
 import 'package:tallee/l10n/generated/app_localizations.dart';
@@ -62,7 +62,8 @@ class MatchFileTile extends StatelessWidget {
                           fit: BoxFit.scaleDown,
                           alignment: Alignment.centerLeft,
                           child: Text(
-                            '${fileName ?? match.name.toSafeFilename()}.$MATCH_FILE_EXTENSION',
+                            fileName ??
+                                '${match.name.toSafeFilename()}.$MATCH_FILE_EXTENSION',
                             maxLines: 1,
                             style: const TextStyle(
                               fontSize: 18,
